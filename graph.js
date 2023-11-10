8db2d7a76fd8aac1fdeea0f9cf73e87676291ff6
johncalabro <j.calabro4@gmail.com>
2023-11-10 11:48:26 -0500

    first

---
+++ graph.js
@@ -0,0 +1,67 @@
+class Node {
+  constructor(value, adjacent = new Set()) {
+    this.value = value;
+    this.adjacent = adjacent;
+  }
+}
+
+class Graph {
+  constructor() {
+    this.nodes = new Set();
+  }
+
+  // this function accepts a Node instance and adds it to the nodes property on the graph
+  addVertex(vertex) {
+    this.nodes.add(vertex)
+  }
+//  this.nodes.add(node)
+  // this function accepts an array of Node instances and adds them to the nodes property on the graph
+  addVertices(vertexArray) {
+    for(let node of vertexArray){
+      this.addVertex(node);
+    }
+  }
+
+  // this function accepts two vertices and updates their adjacent values to include the other vertex
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
+
+  // this function accepts two vertices and updates their adjacent values to remove the other vertex
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
+
+  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
+  removeVertex(vertex) {
+    this.nodes.delete(vertex)
+  }
+
+  // this function returns an array of Node values using DFS
+  depthFirstSearch(start) {
+    // will revisit later on - not much time left in course!
+  }
+
+  // this function returns an array of Node values using BFS
+  breadthFirstSearch(start) {
+     // will revisit later on - not much time left in course!
+  }
+}
+
+const node = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+
+
+const g = new Graph();
+g.addVertex(node)
+g.addVertices([b,c,d,e]);
+g.addEdge(b,c);
+g.removeEdge(b,c);
+g.removeVertex(node);
+
+module.exports = {Graph, Node}
